b0a6e29055443811607f8e4c13b150a570a4be2b
Javier <Javier@DESKTOP-A1FLPSM>
2024-08-26 08:01:44 -0500

    Metodos pago

---
+++ AgriMarket.DTO/PagoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgriMarket.DTO
+{
+    public class PagoDTO
+    {
+        public string MetodoPago { get; set; }
+        public string DetallePago { get; set; }
+    }
+}
